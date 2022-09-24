feeed9f621022a7f9aa0de8cf9c8131692d92ecd
GUchechi <godswilluchendu7@gmail.com>
2022-09-24 20:39:40 +0100

    added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import images from './Images';
 
 function App() {
   return (
     <div className="App">
- 
+        <div className="container">
+          <img src='' alt='selected' className='selected' />
+          <div className="imgContainer">
+            <img src="" alt="dog" />
+          </div>
+        </div>
     </div>
   );
 }

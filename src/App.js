e3c4acc9b263ff0cd1ea45b273550ee1174fe5ca
GUchechi <godswilluchendu7@gmail.com>
2022-09-24 21:32:13 +0100

    image gallery

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import images from './Images';
 
 function App() {
+  const [ selectedImg, setSelectedImg] = useState(images[0])
   return (
     <div className="App">
         <div className="container">
-          <img src='' alt='selected' className='selected' />
+          <img src={selectedImg} alt='selected' className='selected' />
           <div className="imgContainer">
             {
-              images.map((image,index) => <img key={index} src={image} alt='dog'/>)
+              images.map((image,index) => <img 
+              style={{border: selectedImg === image ? '5px solid purple' : ""}}
+              key={index} 
+              src={image} 
+              alt='dog'
+              onClick={() => setSelectedImg(image)} />
+              ) 
             }
           </div>
         </div>
